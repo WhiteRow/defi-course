323c8e7ab6c57bd9e6bc0a504787f8d7018808ce
Egor Belousov <meekgot@gmail.com>
2022-02-19 21:35:32 +0300

    feat(defi-landing): added help redirect

--- src/shared/button/index.jsx
+++ src/shared/button/index.jsx
@@ -1,12 +1,20 @@
+import { socials } from '../../shared/config'
+
 import Button from './ui';
 import TelegramIcon from '../../icons/IconTelegram';
 
 const openCheckoutModal = () => {
   const modal = document.querySelector('.modal');
+  const body = document.body;
   modal.classList.remove('modal--hidden');
+  body.style.overflow = 'hidden';
+}
+
+const openHelp = () => {
+  window.location.href = socials[1].link;
 }
 
 export const BuyButton = () => <Button text="Предзаказ" onClick={openCheckoutModal} />;
 export const HelpButton = () => (
-  <Button text="Написать" type={['secondary', 'small', 'iconed']} icon={TelegramIcon}/>
+  <Button text="Написать" type={['secondary', 'small', 'iconed']} icon={TelegramIcon} onClick={openHelp}/>
 );
