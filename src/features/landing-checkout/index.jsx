e8dfacf81ae8b4f0cb6dd41db24ae83542200962
Egor Belousov <meekgot@gmail.com>
2022-02-20 18:46:38 +0300

    fix(defi-landing): safari fixes#6

--- src/features/landing-checkout/index.jsx
+++ src/features/landing-checkout/index.jsx
@@ -24,11 +24,11 @@ export const LandingCheckout = () => {
               Скидка будет действовать до 18 марта 2022 г.
             </p>
             <div className={styles['buy__old-price']}>
-              <span className={styles.buy__text}>35 000</span>
+              <div className={styles.buy__text}>35 000</div>
               <IconRuble className={styles.buy__icon} />
             </div>
             <div className={styles['buy__price']}>
-              <span className={styles.buy__text}>16 490</span>
+              <div className={styles.buy__text}>16 490</div>
               <IconRuble className={styles.buy__icon} />
             </div>
             <BuyButton handler />
